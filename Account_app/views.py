a9bc416e9a985db49f1416098198b0d85e49bccc
Rudro Dhar <rudro149187@bsdi-bd.org>
2025-05-30 22:25:36 +0600

    get all course

--- Account_app/views.py
+++ Account_app/views.py
@@ -80,57 +80,79 @@ def student_signup(request):
 
 
 def teacher_signup(request):
-    if request.method == 'POST':
-        full_name = request.POST.get("name")
-        username = request.POST.get("username")
-        subject = request.POST.get("subject")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        confirm_password = request.POST.get("confirm_password")
+    if request.user.is_authenticated:
+        return redirect("home")
+    else:
+        if request.method == 'POST':
+            full_name = request.POST.get("name")
+            username = request.POST.get("username")
+            subject = request.POST.get("subject")
+            email = request.POST.get("email")
+            password = request.POST.get("password")
+            confirm_password = request.POST.get("confirm_password")
 
-        if not all([full_name, email, password, subject, username]):
-            messages.error(request, "All field ar required")
-            return render(request, "account/teacher_signup.html")
+            if not all([full_name, email, password, subject, username]):
+                return JsonResponse({
+                    "message" : "All field ar requried",
+                    "success" : False
+                })
 
-        if not password == confirm_password:
-            messages.error(request, "Password and confirm password must be same")
-            return render(request, "account/teacher_signup.html")
-        
-        if User.objects.filter(email=email).exists():
-            messages.error(request , "Email already register")
-            return render(request, "account/teacher_signup.html")
+            if not password == confirm_password:
+                return JsonResponse({
+                    "message" : "Password and confirm password must be same",
+                    "success" : False
+                })
+            
+            if User.objects.filter(email=email).exists():
+                 return JsonResponse({
+                    "message" : "Email already register",
+                    "success" : False
+                })
 
-        
-        if not re.fullmatch(password_regex, password ):
-            messages.error(request ,"write minimum 6 chracter of password. Minimum one capital letter one small letter one digit and one special charecter.")
+            
+            if not re.fullmatch(password_regex, password ):
+               if not re.fullmatch(password_regex, password ):
+                return JsonResponse({
+                    "message" : "write minimum 6 chracter of password. Minimum one capital letter one small letter one digit and one special charecter.",
+                    "success" : False
+                })
+            
+            if not re.fullmatch(name_regex, full_name):
+                return JsonResponse({
+                    "message" : "Please write your full and correct name",
+                    "success" : False
+                })
+            
+            if not re.fullmatch(username_regex,username):
+                return JsonResponse({
+                    "message" : "Start with @ and use one number and always use small letter",
+                    "success" : False
+                })
+            
+            if not re.fullmatch(email_regex, email):
+                return JsonResponse({
+                    "message" : "Please write correct format of email",
+                    "success" : False
+                })
+            
+            user = User(
+                full_name = full_name,
+                username = username,
+                subject = subject,
+                email = email,
+                password=password,
+                is_teacher = True,
+                is_staff = True
+            )
+            user.set_password(password)
+            user.save()
+            # messages.success(request ,"Registration success")
+            return JsonResponse({
+                "success" : True,
+                "message" : "Registration success!! You are registartion as a teacher"
+            })
             return render(request, "account/teacher_signup.html")
-        
-        if not re.fullmatch(name_regex, full_name):
-            messages.error(request ,"Please write your full and correct name")
-            return render(request, "account/teacher_signup.html")
-        
-        if not re.fullmatch(username_regex,username):
-            messages.error(request ,"Start with @ and use one number and always use small letter")
-            return render(request, "account/teacher_signup.html")
-        
-        if not re.fullmatch(email_regex, email):
-            messages.error(request ,"Please write correct format of name")
-            return render(request, "account/teacher_signup.html")
-        
-        user = User(
-            full_name = full_name,
-            username = username,
-            subject = subject,
-            email = email,
-            password=password,
-            is_teacher = True,
-            is_staff = True
-        )
-        user.set_password(password)
-        user.save()
-        messages.success(request ,"Registration success")
         return render(request, "account/teacher_signup.html")
-    return render(request, "account/teacher_signup.html")
 
 
 def user_login(request):
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,14 +1,15 @@
+const nameRegex = /^([A-Za-z]{2,})(\s[A-Za-z]{2,})$/;
+const usernameRegex = /^@([a-z0-9_]{3,})$/;
+const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*_-])[A-Za-z\d!@#$%^&*_-]{6,}/;
+
+// student registration ajax
 $(document).ready(function(){
   console.log("LMS dom is ready student")
 
   $("#student_register").click(function(event){
     event.preventDefault()
     console.log("We ar inside in ready function")
-
-    const nameRegex = /^([A-Za-z]{2,})(\s[A-Za-z]{2,})$/;
-    const usernameRegex = /^@([a-z0-9_]{3,})$/;
-    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*_-])[A-Za-z\d!@#$%^&*_-]{6,}/;
 
     const username = $("#username").val()
     const name = $("#name").val()
@@ -19,22 +20,27 @@ $(document).ready(function(){
 
     if(password !== confirm_password){
         $("#response").html("<p style='color:red'>Password and confirm pasword must be same</p>")
+        return
     }
 
     if(!nameRegex.test(name)){
         $("#response").html("<p style='color:red'>Please Enter correct and full name.</p>")
+        return
     }
 
     if(!emailRegex.test(email)){
         $("#response").html("<p style='color:red'>Please enter correct email</p>")
+        return
     }
 
     if(!usernameRegex.test(username)){
         $("#response").html("<p style='color:red'>Start with @ and use one number and always use small letter</p>")
+        return
     }
 
     if(!passwordRegex.test(password)){
         $("#responser").html("<p style='color:red'>write minimum 6 chracter of password. Minimum one capital letter one small letter one digit and one special charecter.</p>")
+        return
     }
 
     $.ajax({
@@ -64,5 +70,83 @@ $(document).ready(function(){
 
   })
 
+})
+
+// teacher registration ajax
+$(document).ready(function(){
+  console.log("LMS dom is ready Teacher")
+
+  $("#teacher_register").click(function(event){
+    event.preventDefault()
+    console.log("We ar inside in ready function")
+
+    const username = $("#username").val()
+    const name = $("#name").val()
+    const email = $("#email").val()
+    const password = $("#password").val()
+    const confirm_password = $("#confirm_password").val()
+    const subject = $("#subject").val()
+    const csrf_token = $("input[name=csrfmiddlewaretoken]").val();
+
+
+    if(!name || !username || !email || !password || !subject){
+        $("#response").html("<p style='color:red'>All field ar requeried</p>")
+        return
+    }
+
+    if(password !== confirm_password){
+        $("#response").html("<p style='color:red'>Password and confirm pasword must be same</p>")
+        return
+    }
+
+    if(!nameRegex.test(name)){
+        $("#response").html("<p style='color:red'>Please Enter correct and full name.</p>")
+        return
+    }
+
+    if(!emailRegex.test(email)){
+        $("#response").html("<p style='color:red'>Please enter correct email</p>")
+        return
+    }
+
+    if(!usernameRegex.test(username)){
+        $("#response").html("<p style='color:red'>Start with @ and use one number and always use small letter</p>")
+        return
+    }
+
+    if(!passwordRegex.test(password)){
+        $("#response").html("<p style='color:red'>write minimum 6 chracter of password. Minimum one capital letter one small letter one digit and one special charecter.</p>")
+        return
+    }
+
+    $.ajax({
+        url : "/account/teacher_signup/",
+        method : "POST",
+        data : {
+            name : name,
+            username : username,
+            email : email,
+            password : password,
+            confirm_password : confirm_password,
+            subject : subject,
+            csrfmiddlewaretoken: csrf_token
+
+        },
+        success : function(response){
+            if(response.success){
+                $("#response").text(response.message)
+            }else{
+                 $("#response").text("Please try again")
+            }
+        },
+
+        error : function(err){
+            $("#response").err
+        }
+    })
+
+  })
+
 
 })
+
